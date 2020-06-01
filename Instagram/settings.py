06618753353e19cfca835ca6b3a7fa5fe8df4154
virginiah894 <virgyperry@gmail.com>
2020-06-01 09:22:57 +0300

    tinymyc

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -81,11 +81,10 @@ WSGI_APPLICATION = 'Instagram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
+        'NAME': 'Insta',
+        'USER': 'moringa',
+        'PASSWORD': 'eugene',
+
     }
 }
 
