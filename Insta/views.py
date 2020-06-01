c9885f36308a4c96fb3053498f9d374e2d2d63d8
virginiah894 <virgyperry@gmail.com>
2020-06-01 07:15:49 +0300

    Defining login

--- Insta/views.py
+++ Insta/views.py
@@ -1,1 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+frm .models import *
+
+
+
+
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    user = request.user
+    comments = Comment.objects.all()
+    user_profile = Profile.objects.get(user=user)
+    posts = Post.objects.all()
+    profiles = Profile.objects.all()
+    context = {
+        "comments":comments,
+        "posts": posts,
+        "profiles": profiles,
+        "user_profile":user_profile
+    }
+    return render(request, 'index.html', context)
