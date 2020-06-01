3f30a9840db30c728dad6ea811fc1830e1a85de7
virginiah894 <virgyperry@gmail.com>
2020-06-01 08:10:02 +0300

    Configuring respective urls

--- Insta/urls.py
+++ Insta/urls.py
@@ -7,5 +7,10 @@ from django.conf.urls import url
 
 urlpatterns =  [
     url('', views.index, name='index'),
-
+    url('register', views.register, name="register-authentication"),
+    url('accounts/profile/',views.profile_info,name='profile'),
+    url('profile_edit', views.profile_edit, name='profile_edit'),
+    url('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url('search', views.search_results, name='search_results'),
+    url('new_comment/', views.CommentCreateView.as_view(),name='new_comment')
 ]
