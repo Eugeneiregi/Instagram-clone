ebe6d5b742c3d1b7ba0bfaeb73ef205c985804cd
virginiah894 <virgyperry@gmail.com>
2020-06-01 08:13:12 +0300

    Email.multiAlternative

--- Insta/emails.py
+++ Insta/emails.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 def send_welcome_email(name,receiver):
     #creating a message subject and sender
     subject='Welcome to Instagram'
-    sender='gwan.watchlist@gmail.com'
+    sender='eugeneiregi2019@gmail.com.@gmail.com'
 
     #passing in the context variables
     text_content=render_to_string('email/newsemail.txt',{"name":name})
